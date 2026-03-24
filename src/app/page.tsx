7595afd5afbae7278747a71f2f5aded5a88dc1e2
unknown <antuvhai@gmail.com>
2026-03-24 23:53:49 +0600

    medi store client project initialized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export default function Home() {
-  return <h1>hello medi-store</h1>;
+  return (
+    <main>
+      <h1 className="text-4xl font-bold text-center mt-10">Welcome to My Medi Store!</h1>
+      <Button variant="default" className="mt-5">
+        Shop Now
+      </Button>
+    </main>
+  );
 }
