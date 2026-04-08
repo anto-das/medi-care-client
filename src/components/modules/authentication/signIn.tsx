2b92fb447449a68aeaf01ef5bc5d9b658c53786a
unknown <antuvhai@gmail.com>
2026-04-08 10:06:48 +0600

    sign in and sign up form designed

--- src/components/modules/authentication/signIn.tsx
+++ src/components/modules/authentication/signIn.tsx
@@ -8,8 +8,9 @@ import { authClient } from "@/lib/auth-client";
 import { cn } from "@/lib/utils";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { useForm } from "@tanstack/react-form";
 import { FaGoogle } from "react-icons/fa";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
 
 interface Signup2Props {
   heading?: string;
@@ -26,8 +27,17 @@ const SignInPage = ({
   signupUrl = "/sign-up",
   className,
 }: Signup2Props) => {
-  const pathname = usePathname();
-
+  const form = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: ({ value }) => {
+      console.log("Form Values:", value);
+      // Handle form submission logic here
+    },
+  });
   const handleLoginWithGoogle = async () => {
     const res = authClient.signIn.social({
       provider: "google",
@@ -38,70 +48,101 @@ const SignInPage = ({
   // console.log("User Session:", getSession.data);
 
   return (
-    <section className={cn("h-screen bg-muted", className)}>
-      <div className="flex h-full items-center justify-center">
-        <div className="flex flex-col items-center gap-6 lg:justify-start bg-background shadow-md rounded-3xl w-full max-w-lg">
-          {/* Logo */}
-          <div className="bg-[#0c705d] py-10 w-full rounded-t-3xl">
-            <h1 className="text-4xl text-center">🌱</h1>
-            <h2 className="text-3xl font-semibold text-center text-gray-200 py-5">
-              Create Your Account{" "}
-            </h2>
-            <p className="text-center text-gray-300">
-              Join 2 million+ MediCare customers
-            </p>
-          </div>
-          {/* sign up sign in button */}
+    <section
+      className={cn(
+        "min-h-screen flex justify-center items-center bg-muted",
+        className,
+      )}
+    >
+      <div className="flex flex-col lg:flex-row md:flex-row items-center gap-6 lg:justify-evenly  shadow-md rounded-xl lg:rounded-2xl w-11/16 mx-auto bg-[#0c705d] my-10 ">
+        {/* Logo */}
+        <div className="bg-[#0c705d] w-full rounded-lg space-y-5">
+          <h1 className="text-4xl text-center">🌱</h1>
+          <h2 className="text-3xl font-semibold text-center text-gray-200 py-5">
+            Create Your Account{" "}
+          </h2>
+          <p className="text-center text-gray-300">
+            Join 2 million+ MediCare customers
+          </p>
+        </div>
+        {/* sign up sign in button */}
 
-          <div className="w-11/14 mx-auto p-1 flex items-center justify-around bg-[#f8fdfb] border border-[#ddeae7] rounded-md">
-            <Link
-              href={"/sign-in"}
-              className={`font-semibold text-[#738380] ${pathname === "/sign-in" ? "text-[#0c705d] bg-white shadow" : "hover:text-[#0c705d] hover:bg-white"} w-full py-2 text-center rounded-sm`}
-            >
-              Sign in
-            </Link>
-            <Link
-              href={"/sign-up"}
-              className={`font-semibold text-[#738380] ${pathname === "/sign-up" ? "text-[#0c705d] bg-white shadow" : "hover:text-[#0c705d] hover:bg-white"} w-full py-2 text-center rounded-sm`}
-            >
-              Create Account
-            </Link>
-          </div>
-
-          <div className="flex w-full max-w-sm min-w-sm flex-col items-center gap-y-4 rounded-md px-0 py-3">
-            <div className="flex w-full flex-col gap-2">
-              <Label>Email</Label>
-              <Input
-                type="email"
-                placeholder="Email"
-                className="text-sm"
-                required
+        <div className="bg-background p-10 w-full flex flex-col justify-center items-center rounded-b-lg lg:rounded-r-2xl space-y-5">
+          {/* sign up form */}
+          <form
+            id="sign up form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              form.handleSubmit();
+            }}
+            className="w-full"
+          >
+            <FieldGroup className="flex w-full flex-col items-center gap-y-4 rounded-md px-0 py-3 justify-center">
+              <form.Field
+                name="name"
+                children={(field) => {
+                  return (
+                    <Field className="w-11/16 flex-col gap-2">
+                      <FieldLabel className="text-lg font-bold">
+                        Name
+                      </FieldLabel>
+                      <Input
+                        type="text"
+                        placeholder="Name"
+                        className="w-full text-lg py-5 placeholder:text-lg focus:text-lg"
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        required
+                      />
+                    </Field>
+                  );
+                }}
               />
-            </div>
-            <div className="flex w-full flex-col gap-2">
-              <Label>Password</Label>
-              <Input
-                type="password"
-                placeholder="Password"
-                className="text-sm"
-                required
+              <form.Field
+                name="email"
+                children={(field) => {
+                  return (
+                    <Field className="w-11/16 flex-col gap-2">
+                      <FieldLabel className="text-lg font-bold">
+                        Email
+                      </FieldLabel>
+                      <Input
+                        type="email"
+                        placeholder="Email"
+                        className="text-lg py-5 placeholder:text-lg focus:text-lg"
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        required
+                      />
+                    </Field>
+                  );
+                }}
               />
-            </div>
-            <div className="flex w-full flex-col gap-2">
-              <Label>Confirm Password</Label>
-              <Input
-                type="password"
-                placeholder="Password"
-                className="text-sm"
-                required
+              <form.Field
+                name="password"
+                children={(field) => {
+                  return (
+                    <Field className="w-11/16 flex-col gap-2">
+                      <FieldLabel className="text-lg font-bold">
+                        Password
+                      </FieldLabel>
+                      <Input
+                        type="password"
+                        placeholder="Password"
+                        className="text-lg py-5 placeholder:text-lg focus:text-lg"
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        required
+                      />
+                    </Field>
+                  );
+                }}
               />
-            </div>
-            <Button type="submit" className="w-full">
-              {buttonText}
-            </Button>
-          </div>
+            </FieldGroup>
+          </form>
+          <Button type="submit" form="sign up form" className="w-11/16">
+            {buttonText}
+          </Button>
           <Divider />
-          <div className="w-11/15 mx-auto">
+
+          <div className="w-11/16 mx-auto">
             <button
               onClick={handleLoginWithGoogle}
               className="flex items-center gap-2 border border-[#0c705d] text-[#0c705d] hover:bg-[#0c705d] hover:text-white transition-colors duration-300 rounded-md px-4 py-2 w-full justify-center text-lg font-bold hover:text-lg hover:font-bold"
@@ -110,13 +151,14 @@ const SignInPage = ({
               <FaGoogle /> Google{""}
             </button>
           </div>
+
           <div className="flex justify-center items-center gap-1 text-sm text-muted-foreground pb-5">
             <p>{signupText}</p>
             <Link
               href={signupUrl}
               className="font-bold font-[sans-serif] text-[#0c705d]"
             >
-              Sign Up
+              Sign In
             </Link>
           </div>
         </div>
--- src/components/modules/authentication/signup.tsx
+++ src/components/modules/authentication/signup.tsx
@@ -2,12 +2,12 @@
 
 import Divider from "@/components/separator-with-text-1";
 import { Button } from "@/components/ui/button";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
+import { useForm } from "@tanstack/react-form";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useState } from "react";
+
 import { FaGoogle } from "react-icons/fa";
 
 interface Signup2Props {
@@ -20,118 +20,118 @@ interface Signup2Props {
 }
 
 const Signup = ({
-  heading = "Signup",
   buttonText = "Create Account",
   signupText = "Already have an account?",
   signupUrl = "/sign-in",
   className,
 }: Signup2Props) => {
-  const pathname = usePathname();
-  const [userType, setUserType] = useState("customer");
+  const form = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: ({ value }) => {
+      console.log("Form Values:", value);
+      // Handle form submission logic here
+    },
+  });
   return (
-    <section className={cn("h-screen bg-muted", className)}>
-      <div className="flex h-full items-center justify-center">
-        <div className="flex flex-col items-center gap-6 lg:justify-start bg-background shadow-md rounded-3xl w-full max-w-lg">
-          {/* Logo */}
-          <div className="bg-[#0c705d] py-10 w-full rounded-t-3xl">
-            <h1 className="text-4xl text-center">🌱</h1>
-            <h2 className="text-3xl font-semibold text-center text-gray-200 py-5">
-              Create Your Account{" "}
-            </h2>
-            <p className="text-center text-gray-300">
-              Join 2 million+ MediCare customers
-            </p>
-          </div>
-          {/* sign up sign in button */}
+    <section
+      className={cn(
+        "min-h-screen flex justify-center items-center bg-muted",
+        className,
+      )}
+    >
+      <div className="flex flex-col lg:flex-row md:flex-row items-center gap-6 lg:justify-evenly  shadow-md rounded-xl lg:rounded-2xl w-11/16 mx-auto bg-[#0c705d] my-10 ">
+        {/* Logo */}
+        <div className="bg-[#0c705d] w-full rounded-lg space-y-5">
+          <h1 className="text-4xl text-center">🌱</h1>
+          <h2 className="text-3xl font-semibold text-center text-gray-200 py-5">
+            Create Your Account{" "}
+          </h2>
+          <p className="text-center text-gray-300">
+            Join 2 million+ MediCare customers
+          </p>
+        </div>
+        {/* sign up sign in button */}
 
-          <div className="w-11/14 mx-auto p-1 flex items-center justify-around bg-[#f8fdfb] border border-[#ddeae7] rounded-md">
-            <Link
-              href={"/sign-in"}
-              className={`font-semibold text-[#738380] ${pathname === "/sign-in" ? "text-[#0c705d] bg-white shadow" : "hover:text-[#0c705d] hover:bg-white"} w-full py-2 text-center rounded-sm`}
-            >
-              Sign in
-            </Link>
-            <Link
-              href={"/sign-up"}
-              className={`font-semibold text-[#738380] ${pathname === "/sign-up" ? "text-[#0c705d] bg-white shadow" : "hover:text-[#0c705d] hover:bg-white"} w-full py-2 text-center rounded-sm`}
-            >
-              Create Account
-            </Link>
-          </div>
-
-          {/* join holder div */}
-          <div className="w-11/12 mx-auto px-8 rounded-md">
-            <h1 className="py-2 text-[#52421b] text-sm font-semibold">
-              I want to join as
-            </h1>
-            <div className="flex items-center justify-around gap-4 ">
-              <div
-                onClick={() => setUserType("customer")}
-                className={`group text-center rounded-md mx-2 py-4 text-2xl hover:border-2 hover:border-[#0c715e] w-1/4 border-2 transition-colors duration-300 hover:bg-[#f0faf8] ${userType === "customer" ? "border-[#0c715e] bg-[#f0faf8] text-[#0c715e]" : "border-gray-300"}`}
-              >
-                <p>🛒</p>
-                <p className="text-sm  font-semibold group-hover:text-[#0c715e]">
-                  Customer
-                </p>
-              </div>
-              <div
-              
-              onClick={() => setUserType("seller")}
-                className={`group text-center rounded-md mx-2 py-4 text-2xl hover:border-2 hover:border-[#0c715e] w-1/4 border-2 transition-colors duration-300 hover:bg-[#f0faf8] ${userType === "seller" ? "border-[#0c715e] bg-[#f0faf8] text-[#0c715e]" : "border-gray-300"}`}
-              >
-                <p>📦</p>
-                <p className="text-sm  font-semibold group-hover:text-[#0c715e]">
-                  Seller
-                </p>
-              </div>
-              <div
-                onClick={() => setUserType("admin")}
-                className={`group text-center rounded-md mx-2 py-4 text-2xl hover:border-2 hover:border-[#0c715e] w-1/4 border-2 transition-colors duration-300 hover:bg-[#f0faf8] ${userType === "admin" ? "border-[#0c715e] bg-[#f0faf8] text-[#0c715e]" : "border-gray-300"}`}
-              >
-                <p>👮</p>
-                <p className="text-sm  font-semibold group-hover:text-[#0c715e]">
-                  Admin
-                </p>
-              </div>
-            </div>
-          </div>
+        <div className="bg-background p-10 w-full flex flex-col justify-center items-center rounded-b-lg lg:rounded-r-2xl space-y-5">
           {/* sign up form */}
-          <div className="flex w-full max-w-sm min-w-sm flex-col items-center gap-y-4 rounded-md px-0 py-3">
-            <div className="flex w-full flex-col gap-2">
-              <Label>Email</Label>
-              <Input
-                type="email"
-                placeholder="Email"
-                className="text-sm"
-                required
+          <form
+            id="sign up form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              form.handleSubmit();
+            }}
+            className="w-full"
+          >
+            <FieldGroup className="flex w-full flex-col items-center gap-y-4 rounded-md px-0 py-3 justify-center">
+              <form.Field
+                name="name"
+                children={(field) => {
+                  return (
+                    <Field className="w-11/16 flex-col gap-2">
+                      <FieldLabel className="text-lg font-bold">
+                        Name
+                      </FieldLabel>
+                      <Input
+                        type="text"
+                        placeholder="Name"
+                        className="w-full text-lg py-5 placeholder:text-lg focus:text-lg"
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        required
+                      />
+                    </Field>
+                  );
+                }}
               />
-            </div>
-            <div className="flex w-full flex-col gap-2">
-              <Label>Password</Label>
-              <Input
-                type="password"
-                placeholder="Password"
-                className="text-sm"
-                required
+              <form.Field
+                name="email"
+                children={(field) => {
+                  return (
+                    <Field className="w-11/16 flex-col gap-2">
+                      <FieldLabel className="text-lg font-bold">
+                        Email
+                      </FieldLabel>
+                      <Input
+                        type="email"
+                        placeholder="Email"
+                        className="text-lg py-5 placeholder:text-lg focus:text-lg"
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        required
+                      />
+                    </Field>
+                  );
+                }}
               />
-            </div>
-            <div className="flex w-full flex-col gap-2">
-              <Label>Confirm Password</Label>
-              <Input
-                type="password"
-                placeholder="Password"
-                className="text-sm"
-                required
+              <form.Field
+                name="password"
+                children={(field) => {
+                  return (
+                    <Field className="w-11/16 flex-col gap-2">
+                      <FieldLabel className="text-lg font-bold">
+                        Password
+                      </FieldLabel>
+                      <Input
+                        type="password"
+                        placeholder="Password"
+                        className="text-lg py-5 placeholder:text-lg focus:text-lg"
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        required
+                      />
+                    </Field>
+                  );
+                }}
               />
-            </div>
-            <Button type="submit" className="w-full">
-              {buttonText}
-            </Button>
-          </div>
-
+            </FieldGroup>
+          </form>
+          <Button type="submit" form="sign up form" className="w-11/16">
+            {buttonText}
+          </Button>
           <Divider />
 
-          <div className="w-11/15 mx-auto">
+          <div className="w-11/16 mx-auto">
             <button className="flex items-center gap-2 border border-[#0c705d] text-[#0c705d] hover:bg-[#0c705d] hover:text-white transition-colors duration-300 rounded-md px-4 py-2 w-full justify-center text-lg font-bold hover:text-lg hover:font-bold">
               {" "}
               <FaGoogle /> Google{""}
