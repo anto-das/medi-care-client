68d1956c91689391bf6826b96e39fd9304f8ad17
unknown <antuvhai@gmail.com>
2026-05-12 23:08:21 +0600

    updated loading

--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="h-screen flex items-center justify-center font-bold text-gray-400 animate-pulse">
-      Loading Cart...
+      Loading Cart..
     </div>
   );
 };
